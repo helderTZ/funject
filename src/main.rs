2a15d2e19d821be59b2a4ff71873e3838de784d8
Helder Duarte <heldertz2@gmail.com>
2024-03-30 20:51:57 +0000

    Parse methods

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,35 @@ fn main() {
     }
 
     let mut functions: Vec<Entity> = vec![];
+
     for tu in translation_units.iter() {
         functions.extend(
             tu.get_entity().get_children().into_iter().filter(|e| {
                 e.get_kind() == EntityKind::FunctionDecl ||
-                e.get_kind() == EntityKind::FunctionTemplate ||
+                e.get_kind() == EntityKind::FunctionTemplate
             }).collect::<Vec<_>>()
         );
     }
-    //TODO: get classes and get their children to reach the method decls
+
+    for tu in translation_units.iter() {
+        let classes = tu.get_entity().get_children().into_iter().filter(|e| {
+            e.get_kind() == EntityKind::ClassDecl ||
+            e.get_kind() == EntityKind::StructDecl
+        }).collect::<Vec<_>>();
+        for klass in classes.iter() {
+            functions.extend(
+                klass.get_children().into_iter().filter(|e| {
+                    e.get_kind() == EntityKind::Method
+                }).collect::<Vec<_>>()
+            );
+        }
+    }
 
     for function in functions.iter() {
         let location = function.get_location().unwrap().get_expansion_location();
-        println!("function: {:?} @ {:?}:{}:{}:{}",
+        println!("function: {} @ {}:{}:{}:{}",
             function.get_name().unwrap(),
-            location.file.unwrap().get_path(),
+            location.file.unwrap().get_path().as_path().display(),
             location.line,
             location.column,
             location.offset
